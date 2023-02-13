884e1ab3fc5a71eed3f880f1826f7e8cf298cd27
Sam Leavens <3139851+rbwsam@users.noreply.github.com>
2023-02-13 15:16:34 +0100

    WIP

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
+import fileinput
+import os
 import subprocess
+import sys
 
-packages = ['htop', 'vim', 'base-devel', 'otf-codenewroman-nerd', 'alacritty', 'bat']
+packages = [
+    'adobe-source-code-pro-fonts',
+    'alacritty',
+    'base-devel',
+    'bat',
+    'brightnessctl'
+    'chromium',
+    'htop',
+    'vim',
+]
 
 
-def install():
-    subprocess.run(['sudo', 'pacman', '-S', '--needed'] + packages)
+def check_user():
+    if os.geteuid() != 0:
+        sys.exit("error: must be run as root")
 
 
-install()
+def install_packages():
+    subprocess.run(['pacman', '-S', '--noconfirm', '--needed'] + packages)
+
+
+# Replaces commented values in default config (will no-op if config is empty)
+def configure_pacman():
+    with fileinput.input(files='/etc/pacman.conf', inplace=True) as f:
+        for line in f:
+            match line.strip():
+                case '#Color':
+                    print('Color')
+                case l if l.startswith('#ParallelDownloads'):
+                    print('ParallelDownloads = 10')
+                case _:
+                    print(line, end='')
+
+
+check_user()
+configure_pacman()
+install_packages()
